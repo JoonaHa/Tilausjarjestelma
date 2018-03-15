fc1375d7a557f8b9df8d5389cc515354909eb64f
Minä <32388752+JoonaHa@users.noreply.github.com>
2018-03-15 18:45:50 +0200

    Tilausmoottiri is ready

--- Tilanvarausjarjestelma/src/TilausMoottori.java
+++ Tilanvarausjarjestelma/src/TilausMoottori.java
@@ -1,13 +1,63 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author mina
- */
+import dao.Database;
+import dao.VarausDao;
+import domain.Varaus;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
 public class TilausMoottori {
-    
+
+    private static TilausMoottori moottori;
+    private static Database db;
+
+    private TilausMoottori() {
+    }
+
+    public static TilausMoottori getInstance(Database database) {
+        if (moottori == null) {
+            moottori = new TilausMoottori();
+        }
+        db = database;
+        return moottori;
+    }
+
+    protected boolean VaraaTila(Varaus var) throws SQLException, Exception {
+        VarausDao varauksetDao = new VarausDao(db);
+        List<Varaus> varaukset = null;
+        try {
+            varaukset = varauksetDao.findAll();
+        } catch (Exception ex) {
+            System.out.println("Can not list" + varauksetDao.getClass());
+        }
+
+        if (varaukset == null) {
+            return false;
+        }
+
+        if (varaukset.isEmpty()) {
+            varauksetDao.save(var);
+            return true;
+        }
+            // tarkistetaan että varaus alkaa tai loppuu ennen muita varauksia
+        for (int i = 0; i < varaukset.size(); i++) {
+            Varaus get = varaukset.get(i);
+            if (get.getLoppuu().before(var.getAlkaa())
+                    || var.getLoppuu().before(get.getAlkaa())) {
+
+                continue;
+
+            } else {
+                
+                return false;
+            }
+
+        }
+        
+        varauksetDao.save(var);
+        return true;
+    }
+
 }
